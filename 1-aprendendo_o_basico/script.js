f6d16d085d4177d4aa835ded7a55a55c5d81ca62
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-01-05 15:52:31 -0300

    Início do primeiro projeto em JS

---
+++ 1-aprendendo_o_basico/script.js
@@ -0,0 +1,29 @@
+function exibirResultado(){
+let selecionado = document.querySelector('input').value.toLowerCase();
+let pessoa;
+console.log(selecionado);
+if (selecionado == 'cassio'){
+     pessoa = [
+        nome = 'Cássio', sobrenome = 'Ramos', time = 'Corinthians', cpf = '123.456.789-0'
+    ]}
+
+else if (selecionado == 'pedro'){
+     pessoa = [
+    nome = 'Pedro', sobrenome = 'Silva', time = 'Palmeiras', cpf = '234.567.890-1'
+]}
+
+else if (selecionado == 'marcia'){
+     pessoa = [
+    nome = 'Marcia', sobrenome = 'Tiossi', time = 'Corinthians', cpf = '345.678.901-2'
+]}
+
+else if (selecionado == 'carlos'){
+     pessoa = [
+    nome = 'Carlos', sobrenome = 'Tiossi', time = 'Corinthians', cpf = '456.789.012-3'
+]}
+
+console.log(pessoa);    
+}
+
+
+//estilizar ne :p
